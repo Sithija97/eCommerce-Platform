3945019f4aebced99a5fbd360211ea52ef5781ff
Sithija Shehara <nsithijashehara@gmail.com>
2021-07-28 11:23:38 +0530

    serving and fetching products

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -3,17 +3,20 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" exact component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- app-frontend/src/components/Header.js
+++ app-frontend/src/components/Header.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
 import { Container, Navbar, Nav } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Proshop</Navbar.Brand>
+          </LinkContainer>
+
           <Nav className="ml-auto">
-            <Nav.Link href="/cart">
-              <i className="fas fa-shopping-cart"></i> Cart
-            </Nav.Link>
-            <Nav.Link href="/login">
-              <i className="fas fa-user"></i> Sign In
-            </Nav.Link>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <i className="fas fa-user"></i> Sign In
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </Navbar>
