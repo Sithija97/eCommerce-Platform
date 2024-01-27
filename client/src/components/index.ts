dbb468426aba9968c73315614b0bc0474dadbe78
Sithija Shehara <nsithijashehara@gmail.com>
2024-01-27 21:51:42 +0530

    removed client

--- client/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./header";
-export * from "./board.header";
-export * from "./post.form";
-export * from "./button";
-export * from "./login.modal";
-export * from "./register.modal";
-export * from "./text.Input";
